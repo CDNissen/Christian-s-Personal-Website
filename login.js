dc875ae7406348ce91e78a572e5f2d8696cf0585
CDNissen <92cnissen@gmail.com>
2026-07-20 11:59:07 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+const gameText = document.getElementById('game-text2');
+const userInput = document.getElementById('user-input2');
+const submitBtn = document.getElementById('submit-btn2');
+function print(text) {
+  gameText.innerText += text + "\n";
+  gameText.scrollTop = gameText.scrollHeight;
+}
